8a72e4914dfed3212d651eda2b94c144613fbb5a
hirakiuc <hirakiuc@gmail.com>
2021-10-03 20:44:25 +0900

    Simple implementation of the BackendA proxy

---
+++ proto/proxy/api_proxy.pb.go
@@ -0,0 +1,85 @@
+package proxy
+
+import (
+	"context"
+	"fmt"
+
+	A "github.com/hirakiuc/grpc-proxy-sample/proto/A"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+)
+
+func RegisterBackendAHandlerWithBackendOption(ctx context.Context, srv *grpc.Server, target string, opts []grpc.DialOption) (err error) {
+	conn, err := grpc.DialContext(ctx, target, opts...)
+	if err != nil {
+		// nolint:wrapcheck
+		return err
+	}
+
+	defer func() {
+		if err != nil {
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", target, cerr)
+			}
+
+			return
+		}
+
+		go func() {
+			<-ctx.Done()
+
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Info("Failed to close conn to %s: %v", target, cerr)
+			}
+		}()
+	}()
+
+	return RegisterBackendAHandlers(ctx, srv, conn)
+}
+
+func RegisterBackendAHandlers(ctx context.Context, srv *grpc.Server, conn *grpc.ClientConn) error {
+	return RegisterBackendAHandlerWithBackend(ctx, srv, A.NewBackendAClient(conn))
+}
+
+func RegisterBackendAHandlerWithBackend(ctx context.Context, srv *grpc.Server, client BackendAClient) error {
+	backendAServiceDesc := grpc.ServiceDesc{
+		ServiceName: "proxy.BackendA",
+		HandlerType: (*BackendAServer)(nil),
+		Methods: []grpc.MethodDesc{
+			{
+				MethodName: "SayHello",
+				Handler:    _BackendA_SayHello_Handler,
+			},
+		},
+		Streams:  []grpc.StreamDesc{},
+		Metadata: "proxy/api.proto",
+	}
+
+	handlers := newBackendAHandlersImpl(client)
+
+	srv.RegisterService(&backendAServiceDesc, handlers)
+
+	return nil
+}
+
+// handler implementation of a gRPC server for client.
+type backendAHandlersImpl struct {
+	client BackendAClient
+
+	UnimplementedBackendAServer
+}
+
+func newBackendAHandlersImpl(client BackendAClient) BackendAServer {
+	return &backendAHandlersImpl{
+		client: client,
+	}
+}
+
+func (srv *backendAHandlersImpl) SayHello(ctx context.Context, req *A.HelloRequest) (*A.HelloReply, error) {
+	ret, err := srv.client.SayHello(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch a response from backend: %w", err)
+	}
+
+	return ret, nil
+}
--- proxy/server.go
+++ proxy/server.go
@@ -2,18 +2,14 @@ package proxy
 
 import (
 	"context"
-	"strings"
+	"fmt"
 
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
-	grpc_proxy "github.com/mwitkow/grpc-proxy/proxy"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
 
-	pb "github.com/hirakiuc/grpc-proxy-sample/proto/proxy"
+	proxy_pb "github.com/hirakiuc/grpc-proxy-sample/proto/proxy"
 )
 
 type Config struct {
@@ -21,91 +17,27 @@ type Config struct {
 }
 
 func NewServer(conf *Config) (*grpc.Server, error) {
-	director := createDirector()
-
 	opts := []grpc.ServerOption{
-		// https://pkg.go.dev/google.golang.org/grpc#CustomCodec
-		// Deprecated: register codecs using encoding.RegisterCodec.
-		// The server will automatically use registered codecs based on the incoming requests' headers.
-		// See also https://github.com/grpc/grpc-go/blob/master/Documentation/encoding.md#using-a-codec.
-		// Will be supported throughout 1.x.
-		// grpc.CustomCodec(grpc_proxy.Codec()),
-
 		grpc.UnaryInterceptor(grpc_zap.UnaryServerInterceptor(conf.Logger)),
-
-		grpc.UnknownServiceHandler(grpc_proxy.TransparentHandler(director)),
 	}
 
-	return createServer(director, opts...), nil
+	return createServer(opts...)
 }
 
-func createDirector(conf *Config) grpc_proxy.StreamDirector {
-	log := conf.Logger
-
-	return func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
-		backends := map[string]string{
-			"proxy.BackendA": "localhost:50051",
-			"proxy.BackendB": "localhost:50052",
-		}
-
-		const Sep = "/"
-
-		parts := strings.Split(fullMethodName, Sep)
-
-		serviceName := parts[1]
-		methodName := parts[2]
-
-		log.Info(
-			"Receive a request",
-			zap.String("service", serviceName),
-			zap.String("method", methodName),
-		)
-
-		addr, ok := backends[serviceName]
-		if !ok {
-			return nil, nil, status.Errorf(codes.Unimplemented, "Unsupported service")
-		}
-
-		md, ok := metadata.FromIncomingContext(ctx)
-
-		// Copy the inbound metadata explicitly.
-		// nolint:govet
-		outCtx, _ := context.WithCancel(ctx)
-		outCtx = metadata.NewOutgoingContext(outCtx, md.Copy())
-
-		if ok {
-			conn, err := grpc.DialContext(ctx, addr)
-
-			// nolint:wrapcheck
-			return outCtx, conn, err
-		}
-
-		return nil, nil, status.Errorf(codes.Unimplemented, "Unknown method")
-	}
-}
-
-func registerBackendServices(srv *grpc.Server, director grpc_proxy.StreamDirector, backendDescs []grpc.ServiceDesc) {
-	for _, desc := range backendDescs {
-		// Extract backend methods
-		methods := make([]string, len(desc.Methods))
-		for i, v := range desc.Methods {
-			methods[i] = v.MethodName
-		}
-
-		grpc_proxy.RegisterService(srv, director, desc.ServiceName, methods...)
-	}
-}
-
-func createServer(director grpc_proxy.StreamDirector, opts ...grpc.ServerOption) *grpc.Server {
+func createServer(opts ...grpc.ServerOption) (*grpc.Server, error) {
 	srv := grpc.NewServer(opts...)
 
-	backendDescs := []grpc.ServiceDesc{
-		pb.BackendA_ServiceDesc,
-		pb.BackendB_ServiceDesc,
+	ctx := context.Background()
+	clientOptions := []grpc.DialOption{
+		grpc.WithInsecure(),
 	}
 
-	registerBackendServices(srv, director, backendDescs)
+	err := proxy_pb.RegisterBackendAHandlerWithBackendOption(ctx, srv, "localhost:50051", clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to register handler: %w", err)
+	}
+
 	reflection.Register(srv)
 
-	return srv
+	return srv, nil
 }
